070261327208fec034866896e3a0cd4b4cfb74e2
Varun Sharma <varun_sharma@live.co.uk>
2022-09-11 14:38:51 -0700

    feat: integrate auth and chalkra into app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthContextProvider } from '../context/AuthContext';
+import { useRouter } from 'next/router';
+import ProtectedRoute from '../context/ProtectedRoute';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const noAuthRequired = ['/', '/login', '/signup', '/landing', '/categories', '/categories/publications'];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const router = useRouter();
+
+    return (
+        <AuthContextProvider>
+            <ChakraProvider>
+                {noAuthRequired.includes(router.pathname) ? (
+                    <Component {...pageProps} />
+                ) : (
+                    <ProtectedRoute>
+                        <Component {...pageProps} />
+                    </ProtectedRoute>
+                )}
+            </ChakraProvider>
+        </AuthContextProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
